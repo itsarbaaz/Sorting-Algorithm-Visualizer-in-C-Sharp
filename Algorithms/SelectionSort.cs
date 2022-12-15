4756438e055c2d06124044f480b8cdd22c4135d2
Arbaaz Khan <84676972+itsarbaaz@users.noreply.github.com>
2022-12-15 23:30:12 +0530

    Added Iteration Count

--- Algorithms/SelectionSort.cs
+++ Algorithms/SelectionSort.cs
@@ -12,12 +12,12 @@ namespace SAV_1.Algorithms
     {
         private List<Element> elements = new List<Element>();
 
-        public static void Sort(List<Element> elements, Graphics graphics,int tk,int s)
+        public static int Sort(List<Element> elements, Graphics graphics,int tk,int s)
         {
+            int ICount = 0;
             int n=elements.Count;
             for (int i = 0; i < n; i++)
             {
-               
                 int min_idx = i;
                 for (int j = i; j < n; j++)
                 {
@@ -25,33 +25,24 @@ namespace SAV_1.Algorithms
                     if (elements[j].Value < elements[min_idx].Value)
                     {
                         min_idx = j;
+                        ICount++;
+                    }
+                    else
+                    {
+                       ICount++;
                     }
                     ElementHelper.SelectedOne(elements[min_idx], graphics, tk,s);
                 }
                 
                 ElementHelper.DrawSwap(elements[i], elements[min_idx], graphics, tk,s);
                 ElementHelper.ElementSwap(elements, i, min_idx);
-
-
-                /*int min_idx = i;
-
-                 for (int j = i+1; j < n; j++)
-                 {
-                     ElementHelper.SelectedOne(elements[min_idx], graphics, tk);
-                     if (elements[j].Value < elements[min_idx].Value)
-                     {
-                         min_idx = j;
-                     }
-
-                 }
-
-                 ElementHelper.DrawSwap(elements[i], elements[min_idx], graphics, tk);
-                 ElementHelper.ElementSwap(elements, i, min_idx); */
+                ICount++;
             }
             for (int i = 0; i < n; i++)
             {
                 ElementHelper.SelectedOne(elements[i], graphics, tk,s);
             }
+            return ICount;
         }
     }
 }
